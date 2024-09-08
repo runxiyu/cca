4725b6d4b5d554141b2ed5576aaad73ff4833768
Runxi Yu <me@runxiyu.org>
2024-09-08 14:24:43 +0800

    ws.go: Check session cookie
    I'm not sure if all browsers send them along in WebSocket requests.

--- ws.go
+++ ws.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"errors"
 	"net/http"
 
 	"github.com/coder/websocket"
+	"github.com/jackc/pgx/v5"
 )
 
 func handleWs(w http.ResponseWriter, req *http.Request) {
@@ -19,14 +21,34 @@ func handleWs(w http.ResponseWriter, req *http.Request) {
 	}
 	defer c.CloseNow()
 
-	err = handleConn(req.Context(), c)
+	session_cookie, err := req.Cookie("session")
+	if errors.Is(err, http.ErrNoCookie) {
+		c.Write(req.Context(), websocket.MessageText, []byte("U"))
+		return
+	} else if err != nil {
+		c.Write(req.Context(), websocket.MessageText, []byte("E :Error fetching cookie"))
+		return
+	}
+
+	err = handleConn(req.Context(), c, session_cookie.Value)
 	if err != nil {
 		log.Printf("%v", err)
 		return
 	}
 }
 
-func handleConn(ctx context.Context, c *websocket.Conn) error {
+func handleConn(ctx context.Context, c *websocket.Conn, session string) error {
+	var userid string
+	var expr int
+	err := db.QueryRow(context.Background(), "SELECT userid, expr FROM sessions WHERE cookie = $1", session).Scan(&userid, &expr)
+	if errors.Is(err, pgx.ErrNoRows) {
+		c.Write(ctx, websocket.MessageText, []byte("U"))
+		return err
+	} else if err != nil {
+		c.Write(ctx, websocket.MessageText, []byte("E :Database error"))
+		return err
+	}
+
 	for {
 		typ, b, err := c.Read(ctx)
 		if err != nil {
