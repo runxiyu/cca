5437a9ae768c5ba540f76f4485c6d89235db0b5d
Runxi Yu <me@runxiyu.org>
2024-09-08 13:47:42 +0800

    tcourse.sql: Test SQL file to add random courses

---
+++ tcourse.sql
@@ -0,0 +1,6 @@
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(1, 'A', 'Sport', 'T', 'L', 20);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(2, 'B', 'Sport', 'T', 'L', 30);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(3, 'C', 'Sport', 'T', 'L', 10);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(4, 'D', 'Non-sport', 'T', 'L', 10);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(5, 'E', 'Non-sport', 'T', 'L', 6);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(6, 'F', 'Non-sport', 'T', 'L', 40);
