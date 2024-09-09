d31ba12d49f00b639bcea2c8d254953c4608b657
Runxi Yu <me@runxiyu.org>
2024-09-09 17:06:12 +0800

    tcourse.sql: Make the names slightly less random

--- tcourse.sql
+++ tcourse.sql
@@ -1,6 +1,6 @@
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(1, 'A', 'Sport', 'T', 'L', 20);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(2, 'B', 'Sport', 'T', 'L', 30);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(3, 'C', 'Sport', 'T', 'L', 10);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(4, 'D', 'Non-sport', 'T', 'L', 10);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(5, 'E', 'Non-sport', 'T', 'L', 6);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(6, 'F', 'Non-sport', 'T', 'L', 40);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(1, 'Alpha', 'Sport', 'Golf', 'L', 20);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(2, 'Bravo', 'Sport', 'Hotel', 'L', 30);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(3, 'Charlie', 'Enrichment', 'India', 'L', 10);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(4, 'Delta', 'Enrichment', 'Juliet', 'L', 10);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(5, 'Echo', 'Culture', 'Kilo', 'L', 6);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(6, 'Foxtrot', 'Culture', 'Lima', 'L', 40);
