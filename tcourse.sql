5f92ce0600b99e9f7e871441ed3abcf91f0bff89
Runxi Yu <me@runxiyu.org>
2024-09-12 23:45:01 +0800

    tcourse.sql: Use real-ish example data

--- tcourse.sql
+++ tcourse.sql
@@ -1,6 +1,5 @@
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(1, 'Alpha', 'Sport', 'Golf', 'L', 20);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(2, 'Bravo', 'Sport', 'Hotel', 'L', 30);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(3, 'Charlie', 'Enrichment', 'India', 'L', 10);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(4, 'Delta', 'Enrichment', 'Juliet', 'L', 10);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(5, 'Echo', 'Culture', 'Kilo', 'L', 6);
-INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(6, 'Foxtrot', 'Culture', 'Lima', 'L', 40);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(1, 'Floorball', 'Sport', 'Chen Xiaojia', 'Gym', 20);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(2, 'Flag Football', 'Sport', 'Ding Zhaoyuan', 'Pitch', 30);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(3, 'Chamber Music', 'Enrichment', '(?)', 'Music Rooms', 10);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(4, 'Math and Computer Science', 'Enrichment', 'Jeff Zhang', '2307', 10);
+INSERT INTO courses (id, title, ctype, teacher, location, nmax) VALUES(5, 'Chinese Drama', 'Culture', 'Monica Chen (?)', 'Black Box (?)', 6);
