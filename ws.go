d209a85a1614254ca5171deaa8f11038d71e72ec
Runxi Yu <me@runxiyu.org>
2024-09-12 08:56:29 +0800

    ws.go: Document the message format

--- ws.go
+++ ws.go
@@ -28,6 +28,31 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+
+/*
+ * The message format is a WebSocket message separated with spaces.
+ * The contents of each field could contain anything other than spaces,
+ * null bytes, carriage returns, and newlines. The first character of
+ * each argument cannot be a colon. As an exception, the last argument may
+ * contain spaces and the first character thereof may be a colon, if the
+ * argument is prefixed with a colon. The colon used for the prefix is not
+ * considered part of the content of the message. For example, in
+ *
+ *    SQUISH POP :cat purr!!
+ *
+ * the first field is "SQUISH", the second field is "POP", and the third
+ * field is "cat purr!!".
+ *
+ * It is essentially an RFC 1459 IRC message without trailing CR-LF and
+ * without prefixes. See section 2.3.1 of RFC 1459 for an approximate
+ * BNF representation.
+ * 
+ * The reason this was chosen instead of using protobuf etc. is that it
+ * is simple to parse without external libraries, and it also happens to
+ * be a format I'm very familiar with, having extensively worked with the
+ * IRC protocol.
+ */
+
 package main
 
 import (
