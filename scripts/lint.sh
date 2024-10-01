bcea7643c5d61d543d0d15eebf2490922e479262
Runxi Yu <me@runxiyu.org>
2024-10-01 16:43:47 +0800

    lint.sh: Disable yet another cyclomatic complexity checker

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-golangci-lint run --color=always --enable-all --disable=wsl,funlen,exportloopref,gomnd,execinquery,godox,lll,gochecknoglobals,depguard,cyclop,gosmopolitan,nlreturn,varnamelen,nestif,musttag,mnd,tagliatelle,gocognit,gocyclo
+golangci-lint run --color=always --enable-all --disable=wsl,funlen,exportloopref,gomnd,execinquery,godox,lll,gochecknoglobals,depguard,cyclop,gosmopolitan,nlreturn,varnamelen,nestif,musttag,mnd,tagliatelle,gocognit,gocyclo,maintidx
