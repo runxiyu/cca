0ceea7caf8344ea8d93ffe0c0cd71589fdc354a3
Runxi Yu <me@runxiyu.org>
2024-10-01 12:00:16 +0800

    postgres_run.sh: Run in debug level 2
    This allows SQL statements to be logged which makes debugging much
    easier. Also "$@" is added so any extra arguments may be passed.

--- scripts/postgres_run.sh
+++ scripts/postgres_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-LC_ALL="C" exec /opt/homebrew/opt/postgresql@15/bin/postgres -D /opt/homebrew/var/postgresql@15
+LC_ALL="C" exec /opt/homebrew/opt/postgresql@15/bin/postgres -D /opt/homebrew/var/postgresql@15 -d 2 "$@"
