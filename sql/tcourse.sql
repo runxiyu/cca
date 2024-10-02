c0e1c1ef272bb36985856f1647b9f19d92308a7e
Runxi Yu <me@runxiyu.org>
2024-10-02 13:51:18 +0800

    tcourse.sql: Add 1000 of each course for testing

--- sql/tcourse.sql
+++ sql/tcourse.sql
@@ -1,5 +1,5 @@
-INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Floorball', 'Sport', 'Chen Xiaojia', 'Gym', 20);
-INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Flag Football', 'Sport', 'Ding Zhaoyuan', 'Pitch', 30);
-INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Chamber Music', 'Enrichment', '(?)', 'Music Rooms', 10);
-INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Math and Computer Science', 'Enrichment', 'Jeff Zhang', '2307', 10);
-INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Chinese Drama', 'Culture', 'Monica Chen (?)', 'Black Box (?)', 6);
+INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Floorball', 'Sport', 'Chen Xiaojia', 'Gym', 1000);
+INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Flag Football', 'Sport', 'Ding Zhaoyuan', 'Pitch', 1000);
+INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Chamber Music', 'Enrichment', '(?)', 'Music Rooms', 1000);
+INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Math and Computer Science', 'Enrichment', 'Jeff Zhang', '2307', 1000);
+INSERT INTO courses (title, ctype, teacher, location, nmax) VALUES('Chinese Drama', 'Culture', 'Monica Chen (?)', 'Black Box (?)', 1000);
