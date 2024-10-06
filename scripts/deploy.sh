a6d4089a86cfc438a6350977f542639d50b1754b
Runxi Yu <me@runxiyu.org>
2024-10-06 17:24:07 +0800

    deploy.sh: Add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -xeu
+
+home_dir="${BASH_SOURCE[0]%/*}/.."
+cd "$home_dir"
+
+make build/static/student.js
+gsed -i 's;wss://localhost.runxiyu.org:8080/ws;wss://dev.runxiyu.org/ws;g' build/static/student.js
+
+mv dist/cca dist/ccae
+GOOS=linux GOARCH=amd64 make dist/cca
+
+rm build/static/student.js
+
+rsync -v dist/cca root@runxiyu.org:/srv/dev/cca
+
+ssh root@runxiyu.org pkill cca
