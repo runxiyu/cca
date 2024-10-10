67658da2ffa69f55dff57d34229cb47fa66e3198
Runxi Yu <me@runxiyu.org>
2024-10-10 23:50:14 +0800

    deploy.sh: Update sed expression

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ home_dir="${BASH_SOURCE[0]%/*}/.."
 cd "$home_dir"
 
 make build/static/student.js
-gsed -i 's;wss://localhost.runxiyu.org:8080/ws;wss://dev.runxiyu.org/ws;g' build/static/student.js
+gsed -i 's;wss://cca.runxiyu.org/ws;wss://dev.runxiyu.org/ws;g' build/static/student.js
 
 mv dist/cca dist/ccae
 GOOS=linux GOARCH=amd64 make dist/cca
