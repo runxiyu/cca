80b34a67b36cfe5de7763e4f505f4d7f91b94ed9
Runxi Yu <me@runxiyu.org>
2024-10-11 20:55:13 +0800

    iadocs: Appendix update

--- scripts/latexify-source.sh
+++ scripts/latexify-source.sh
@@ -13,35 +13,35 @@ printfile() {
 	shift 3
 	for i in "$@"
 	do
-		printf '\\subsection{%s}\n' "$(sed 's/_/\\_/g' <<< "$i")" >> "$targetfile"
+		printf '\\section{%s}\n' "$(sed 's/_/\\_/g' <<< "$i")" >> "$targetfile"
 		printf '\\inputminted[breaklines, tabsize=%s]{%s}{%s/%s}\n' "$tabsize" "$lang" "$base" "$i" >> "$targetfile"
 	done
 }
 
-printf '\\section{Backend source code}\n' >> "$targetfile"
+printf '\\chapter{Backend source code}\n' >> "$targetfile"
 printfile go 8 ./ *.go
 printfile text 8 ./ go.*
 
-printf '\\section{Frontend source code}\n' >> "$targetfile"
+printf '\\chapter{Frontend source code}\n' >> "$targetfile"
 cd frontend
 printfile javascript 4 ./frontend *.js
 printfile css 8 ./frontend *.css
 
-printf '\\section{HTML templates}\n' >> "$targetfile"
+printf '\\chapter{HTML templates}\n' >> "$targetfile"
 cd ../templates
 printfile html 2 ./templates *.html
 
-printf '\\section{Build system and auxiliary scripts}\n' >> "$targetfile"
+printf '\\chapter{Build system and auxiliary scripts}\n' >> "$targetfile"
 cd ..
 printfile makefile 8 ./ Makefile
 cd scripts
 printfile bash 8 ./scripts *.sh
 
-printf '\\section{SQL scripts}\n' >> "$targetfile"
+printf '\\chapter{SQL scripts}\n' >> "$targetfile"
 cd ../sql
 printfile postgresql 8 ./sql *.sql
 
-printf '\\section{Production documentation}\n' >> "$targetfile"
+printf '\\chapter{Production documentation}\n' >> "$targetfile"
 cd ../docs
 printfile html 2 ./docs *.html
 printfile css 8 ./docs *.css
