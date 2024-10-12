5b96cff9f9976801ce61f8ef8b6fdd620312608c
Runxi Yu <me@runxiyu.org>
2024-10-12 14:24:58 +0800

    latexify-source.sh: Don't do the relative path stuff

--- scripts/latexify-source.sh
+++ scripts/latexify-source.sh
@@ -9,44 +9,36 @@ printf '\n' > "$targetfile"
 printfile() {
 	lang="$1"
 	tabsize="$2"
-	base="$3"
-	shift 3
+	shift 2
 	for i in "$@"
 	do
 		printf '\\section{%s}\n' "$(sed 's/_/\\_/g' <<< "$i")" >> "$targetfile"
-		printf '\\inputminted[breaklines, tabsize=%s]{%s}{%s/%s}\n' "$tabsize" "$lang" "$base" "$i" >> "$targetfile"
+		printf '\\inputminted[breaklines, tabsize=%s]{%s}{%s}\n' "$tabsize" "$lang" "$i" >> "$targetfile"
 	done
 }
 
 printf '\\chapter{Backend source code}\n' >> "$targetfile"
-printfile go 8 ./ *.go
-printfile text 8 ./ go.*
+printfile go 8 *.go
+printfile text 8 go.*
 
 printf '\\chapter{Frontend source code}\n' >> "$targetfile"
-cd frontend
-printfile javascript 4 ./frontend *.js
-printfile css 8 ./frontend *.css
+printfile javascript 4 frontend/*.js
+printfile css 8 frontend/*.css
 
 printf '\\chapter{HTML templates}\n' >> "$targetfile"
-cd ../templates
-printfile html 2 ./templates *.html
+printfile html 2 templates/*.html
 
 printf '\\chapter{Build system and auxiliary scripts}\n' >> "$targetfile"
-cd ..
-printfile makefile 8 ./ Makefile
-cd scripts
-printfile bash 8 ./scripts *.sh
+printfile makefile 8 Makefile
+printfile bash 8 scripts/*.sh
 
 printf '\\chapter{SQL scripts}\n' >> "$targetfile"
-cd ../sql
-printfile postgresql 8 ./sql *.sql
+printfile postgresql 8 sql/*.sql
 
 printf '\\chapter{Production documentation}\n' >> "$targetfile"
-cd ../docs
-printfile html 2 ./docs *.html
-printfile css 8 ./docs *.css
-printfile text 8 ./docs *.csv cca.scfg.example
+printfile html 2 docs/*.html
+printfile css 8 docs/*.css
+printfile text 8 docs/*.csv docs/cca.scfg.example
 
 printf '\\chapter{IA documentation}\n' >> "$targetfile"
-cd ../iadocs
-printfile latex 8 ./iadocs *.tex *.texinc
+printfile latex 8 iadocs/*.tex iadocs/*.texinc
